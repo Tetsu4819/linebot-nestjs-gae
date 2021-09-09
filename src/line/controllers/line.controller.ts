c16111f77a4da64bef5d510e7e06177e4d9de25d
tetsu4819 <tetsu4819@gmail.com>
2021-09-10 07:53:31 +0900

    update line.controllers

--- src/line/controllers/line.controller.ts
+++ src/line/controllers/line.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { WebhookRequestBody } from '@line/bot-sdk';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { LineHandleEvent } from '../line-handlers/event.handler';
 
 @Controller('line')
-export class LineController {}
+export class LineController {
+  constructor(private readonly LineHandleEvent: LineHandleEvent) {}
+  @Get()
+  helloLine(): string {
+    return 'Hello Line';
+  }
+  @Post('webhook')
+  async lineWebhook(@Body() { events }: WebhookRequestBody): Promise<any> {
+    return this.LineHandleEvent.handleEvent(events);
+  }
+}
