c177d1c5a00e122c01814089f854c3e79c812988
tetsu4819 <tetsu4819@gmail.com>
2021-09-10 07:59:54 +0900

    import handlers in line.modules

--- src/line/line.module.ts
+++ src/line/line.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { LineController } from './controllers/line.controller';
+import { LineHandleEvent } from './line-handlers/event.handler';
+import { FollowHandler } from './line-handlers/follow/follow.handler';
+import { MessageHandler } from './line-handlers/message/message.handler';
+import { AudioHandler } from './line-handlers/message/types/audio.handler';
+import { ImageHandler } from './line-handlers/message/types/image.handler';
+import { LocationHandler } from './line-handlers/message/types/location.handler';
+import { StickerHandler } from './line-handlers/message/types/sticker.handler';
+import { TextHandler } from './line-handlers/message/types/text.handler';
+import { PostbackHandler } from './line-handlers/postback/postback.handler';
 import { LineService } from './services/line.service';
 
 @Module({
   controllers: [LineController],
-  providers: [LineService],
+  providers: [
+    LineService,
+    LineHandleEvent,
+    MessageHandler,
+    TextHandler,
+    ImageHandler,
+    AudioHandler,
+    LocationHandler,
+    StickerHandler,
+    PostbackHandler,
+    LineController,
+    FollowHandler,
+  ],
 })
 export class LineModule {}
